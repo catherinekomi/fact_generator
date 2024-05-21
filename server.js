707062c0ac23c3e8b2355ac3ce5967e5c6e03995
Catherine Komi <oceanbeaats@gmail.com>
2024-05-21 15:42:32 -0600

    adjust server.js

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const PORT = 5000;
 
+app.use(cors());
 app.use(express.json());
+
+const API_KEY = process.env.API_KEY;
 
 app.post('/generate-interesting-fact', async (req, res) => {
   const { animal } = req.body;
-  const userMessage = `Create an interesting fact about the ${animal}, 2 sentences only.`;
+  const userMessage = `Create an interesting fact about the ${animal}.`;
 
   try {
     const userResponse = await axios.post(
       'https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct',
       { inputs: userMessage },
-      { headers: { Authorization: `Bearer ${process.env.API_KEY}` } }
+      { headers: { Authorization: `Bearer ${API_KEY}` } }
     );
 
     const llamaMessage = userResponse.data[0].generated_text;
 
-    res.json({ llamaMessage });
+    res.json({ userMessage, llamaMessage });
   } catch (error) {
     console.error('Error generating interesting fact:', error);
     res.status(500).json({ error: 'Failed to generate interesting fact' });
   }
 });
 
-const PORT = process.env.PORT || 3000; // Use the provided port or a default one
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-module.exports = app;
