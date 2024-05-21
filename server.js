e7abfc75513e0cfbd8e1aaa0470336a1546191fb
Catherine Komi <oceanbeaats@gmail.com>
2024-05-21 16:18:22 -0600

    added client folder

--- server.js
+++ server.js
@@ -1,36 +1,40 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-const API_KEY = process.env.API_KEY;
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.post('/generate-interesting-fact', async (req, res) => {
   const { animal } = req.body;
-  const userMessage = `Create an interesting fact about the ${animal}.`;
+  const userMessage = `Create an interesting fact about the ${animal}, 2 sentences only and no more than 200 characters and no hashtags.`;
 
   try {
     const userResponse = await axios.post(
       'https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct',
       { inputs: userMessage },
-      { headers: { Authorization: `Bearer ${API_KEY}` } }
+      { headers: { Authorization: `Bearer ${process.env.API_KEY}` } }
     );
 
     const llamaMessage = userResponse.data[0].generated_text;
 
-    res.json({ userMessage, llamaMessage });
+    res.json({ llamaMessage });
   } catch (error) {
     console.error('Error generating interesting fact:', error);
     res.status(500).json({ error: 'Failed to generate interesting fact' });
   }
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
