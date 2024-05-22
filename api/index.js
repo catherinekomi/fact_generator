15851630ee9a903e4440f00df0ad67b17dfc58e0
Catherine Komi <oceanbeaats@gmail.com>
2024-05-22 10:25:53 -0600

    Vercel api structure

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.post('/generate-interesting-fact', async (req, res) => {
   const { animal } = req.body;
@@ -32,9 +32,7 @@ app.post('/generate-interesting-fact', async (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
