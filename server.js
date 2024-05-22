0d8a81eb4f3738b72a4daef4b21e961472b83757
Catherine Komi <oceanbeaats@gmail.com>
2024-05-22 13:13:41 -0600

    CORS

--- server.js
+++ server.js
@@ -7,7 +7,20 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = ['https://fact-generator-coral.vercel.app'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/build')));
 
