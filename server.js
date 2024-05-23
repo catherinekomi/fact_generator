b2d1f0df34c84de72068973945af24da6923d4a7
Catherine Komi <oceanbeaats@gmail.com>
2024-05-22 18:17:30 -0600

    update server.js

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ app.get('*', (req, res) => {
     }
   });
 });
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
